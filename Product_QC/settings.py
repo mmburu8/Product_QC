24c041b95ad9a260a680f0fde4486c0a93614d64
mmburu8 <mmburu961@gmail.com>
2024-04-26 17:04:40 +0300

    yesterday

--- Product_QC/settings.py
+++ Product_QC/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_z@@ah%7aann=(29+94y*tajz@4@2))()-n1penfg2sn_z54eb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'Product_QC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR /"db.sqlite3"
+    #}
+#}
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -89,7 +89,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "mwfCSRFIbDixUWcZPlnTeBtJPoqFuvtd",
         "HOST": "roundhouse.proxy.rlwy.net",
-        "PORT": 46625
+        "PORT": '46625'
     }
 }
 
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # List of directories where Django will look for additional static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'product_kpi'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Product_QC/wsgi.py
+++ Product_QC/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Product_QC.settings")
 application = get_wsgi_application()
 app = application
 
