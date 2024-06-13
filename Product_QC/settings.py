449df1b090e70556585d85c0b751d35a7467a313
mmburu8 <mmburu961@gmail.com>
2024-06-13 09:20:50 +0300

    wickedest

--- Product_QC/settings.py
+++ Product_QC/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_z@@ah%7aann=(29+94y*tajz@4@2))()-n1penfg2sn_z54eb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost", "product-qc.onrender.com"]
 
 
 # Application definition
